7d7657b3d30cb9e11d2155db73fb64edb7d87e13
Jessica Gonzales <jgonzales0688@gmail.com>
2022-12-15 22:40:32 -0600

    Week 1 done

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +10,73 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            if (c == char.MinValue) return false; return true;
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if(vals.Length % 2 == 0) return true; return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if(number % 2 == 0) return true; return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0) return true; return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            double MaxValue = numbers.Max();
+            double MinValue = numbers.Min();
+            return MaxValue + MinValue;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if(str1.Length <str2.Length) return str1.Length; return str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int total = numbers.Sum(x => Convert.ToInt32(x));
+                return total;
+            }
+           
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+                    { return 0; }
+            else
+            {
+                var total = numbers.Where(i => i % 2 == 0).Sum();
+                return total;
+            }
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            int total = numbers.Sum(x => Convert.ToInt32(x));
+            if (numbers == null)
+            { 
+                return false; 
+            }
+            else
+            {
+              int total = numbers.Sum(x => Convert.ToInt32(x));
+                    return true;
+            }
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
